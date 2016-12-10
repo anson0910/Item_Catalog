b88609b5260ab44d51c06625a003b231b1333516
Anson <anson32330200@gmail.com>
2016-12-09 19:42:13 -0800

    Init commit

---
+++ project.py
@@ -0,0 +1,82 @@
+from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
+from sqlalchemy import create_engine, asc
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, User, Category, Item
+from flask import session as login_session
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+import json
+from flask import make_response
+import requests
+
+from apis.UserAPI import user_api
+
+app = Flask(__name__)
+app.register_blueprint(user_api)
+
+CLIENT_ID = json.loads(
+    open('client_secrets.json', 'r').read())['web']['client_id']
+APPLICATION_NAME = "Item Catalog Application"
+
+
+# Connect to Database and create database session
+engine = create_engine('sqlite:///catalogwithusers.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+# Show all items
+@app.route('/')
+@app.route('/items/')
+def showAllItems():
+    categories = session.query(Category).order_by(asc(Category.id))
+    items = session.query(Item).order_by(asc(Item.id))
+    return "hello"
+
+
+@app.route('/catalog/<path:category_name>/items/')
+def showCategoryItems(category_name):
+    category = session.query(Category).filter_by(name=category_name).one()
+    items = session.query(Item).filter_by(category=category).order_by(asc(Item.id))
+    for i in items:
+        print i.name
+    return "hello"
+
+
+@app.route('/catalog/<path:category_name>/<path:item_name>/')
+def showItem(category_name, item_name):
+    item = session.query(Item).filter_by(name=item_name).one()
+    print item.name
+    return "hello"
+
+
+if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
+
+
+
+
+# # JSON APIs to view Restaurant Information
+# @app.route('/restaurant/<int:restaurant_id>/menu/JSON')
+# def restaurantMenuJSON(restaurant_id):
+#     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+#     items = session.query(MenuItem).filter_by(
+#         restaurant_id=restaurant_id).all()
+#     return jsonify(MenuItems=[i.serialize for i in items])
+#
+#
+# @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/JSON')
+# def menuItemJSON(restaurant_id, menu_id):
+#     Menu_Item = session.query(MenuItem).filter_by(id=menu_id).one()
+#     return jsonify(Menu_Item=Menu_Item.serialize)
+#
+#
+# @app.route('/restaurant/JSON')
+# def restaurantsJSON():
+#     restaurants = session.query(Restaurant).all()
+#     return jsonify(restaurants=[r.serialize for r in restaurants])
