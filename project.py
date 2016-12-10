614531ac41f7beed4e456af7ce3aba85adaffbb0
Anson <anson32330200@gmail.com>
2016-12-09 22:25:35 -0800

    Add links for categories and items

--- project.py
+++ project.py
@@ -34,7 +34,7 @@ session = DBSession()
 def showAllItems():
     categories = session.query(Category).order_by(asc(Category.id))
     items = session.query(Item).order_by(asc(Item.id))
-    return render_template('all_items.html', categories=categories, items=items)
+    return render_template('items_all.html', categories=categories, items=items)
 
 
 @app.route('/catalog/<path:category_name>/items/')
